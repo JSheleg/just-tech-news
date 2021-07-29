85b38b82e2e53f9a7d7193fcd59abd93fe150e8e
JSheleg <jessica.sheleg@gmail.com>
2021-07-29 16:48:15 -0500

    post model created

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //used for collecting and exporting the USER model data
 
 const User = require('./User');
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
