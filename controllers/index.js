16ca7965b0549598f5a0b478f6a957179c9e0993
JSheleg <jessica.sheleg@gmail.com>
2021-08-03 15:08:28 -0500

    Adding MVC protocalls

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 //collecting packaged group of API endpoints and prefixin them with the path /api
 const router = require('express').Router();
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+//set up handlebars as template engine part 1
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//express.static takes all the content of the folder and serves them as static assets. 
+//usefull for Front End files like img, css and js
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+//set up handlesbars as template engine part 2
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 //true allows the tables to re-create if there are any association changes
